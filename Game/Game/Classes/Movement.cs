b89e378150bac3314a7d734602cd288180149d5b
András Szin <83217108+dernox47@users.noreply.github.com>
2023-04-30 20:01:31 +0200

    Mozgás
    Movement class meg lett csinálva, a karakter tud mozogni 2D-sen a konzolba.
    Létre lett hozva egy Sprites osztály amiben egyelőre nem használt animációk vannak. Talán majd egyszer

---
+++ Game/Game/Classes/Movement.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Classes
+{
+    class Movement
+    {
+        private int x;
+        private int y;
+
+        public Movement(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(@" o ");
+            Console.SetCursorPosition(x, y + 1);
+            Console.WriteLine(@"/|\");
+            Console.SetCursorPosition(x, y + 2);
+            Console.WriteLine(@"/ \");
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(@" o ");
+            Console.SetCursorPosition(x, y + 1);
+            Console.WriteLine(@"/|\");
+            Console.SetCursorPosition(x, y + 2);
+            Console.WriteLine(@"/ \");
+        }
+        public void Remove()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(@"   ");
+            Console.SetCursorPosition(x, y + 1);
+            Console.WriteLine(@"   ");
+            Console.SetCursorPosition(x, y + 2);
+            Console.WriteLine(@"   ");
+        }
+
+        public void MoveLeft()
+        {
+            if (x > 0)
+            {
+                Remove();
+                x -= 2;
+                Draw();
+            }
+        }
+
+        public void MoveRight()
+        {
+            if (x < Console.WindowWidth - 4)
+            {
+                Remove();
+                x += 2;
+                Draw();
+            }
+        }
+
+        public void MoveUp()
+        {
+            if (y > 0)
+            {
+                Remove();
+                y--;
+                Draw();
+            }
+        }
+
+        public void MoveDown()
+        {
+            if (y < Console.WindowHeight - 4)
+            {
+                Remove();
+                y++;
+                Draw();
+            }
+        }
+    }
+}
+
--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -20,14 +20,37 @@ namespace Game
             //inventory.AddItem("poti", 4);
             //inventory.AddItem("kard");
             //inventory.OpenInventory();
-            //inventory.RemoveItem("poti", 2);
+            //inventory.RemoveItem("poti", );
             //inventory.RemoveItem("kard");
             //inventory.OpenInventory();
 
 
-            MainGame game = new MainGame();
+            //MainGame game = new MainGame();
+            //game.Start();
 
-            game.Start();
+            Movement player = new Movement(0, 0);
+
+            while (true)
+            {
+                Console.CursorVisible = false;
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        player.MoveLeft();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        player.MoveRight();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        player.MoveUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        player.MoveDown();
+                        break;
+                }
+            }
 
             Console.ReadKey();
         }
